82030cf84f0112d295a8aeb5a30d81765a65fa06
erin0418 <erinf0418@gmail.com>
2018-08-23 09:51:35 -0400

    heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 8889,
-    user: "root",
-    password: "root",
-    database: "weddingPlanner_DB"
-  });
+var connection;
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'weddingPlanner_DB'
+  })
+}
   connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
